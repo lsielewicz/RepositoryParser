e02237b4fdf2245b8e4e55db85686201b5ca8265
Lukasz Sielewicz <lsielewicz@wi.zut.edu.pl>
2016-03-16 11:44:27 +0100

    Added: GitRepository class and GitRepositoryTable class

---
+++ RepositoryParser/RepositoryParser.Core/Models/GitRepositoryTable.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RepositoryParser.Core.Models
+{
+    public class GitRepositoryTable
+    {
+        public int ID_Repository { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public GitRepositoryTable()
+        {
+            ID_Repository = 0;
+        }
+
+        public GitRepositoryTable(int rnumber)
+        {
+            ID_Repository = rnumber;
+        }
+
+        public GitRepositoryTable(string name)
+        {
+            Name = name;
+        }
+
+        public GitRepositoryTable(int ID, string name)
+        {
+            ID_Repository = ID;
+            Name = name;
+        }
+        public GitRepositoryTable(int ID, string name, string type)
+        {
+            ID_Repository = ID;
+            Name = name;
+            Type = type;
+        }
+
+
+        public static string createTable = "CREATE TABLE Repository(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name varchar(40), Type varchar(40))";
+        public static string deleteAllQuery = "DELETE FROM Repository";
+
+        public static string InsertQuery(GitRepositoryTable obj)
+        {
+            obj.Name = GitRepositoryTable.FixName(obj.Name);
+            string query = "INSERT INTO Repository (Name,Type) Values ('" + obj.Name + "', 'Git')";
+            return query;
+        }
+
+        public List<GitRepositoryTable> GetDataFromBase(SQLiteConnection Connection)
+        {
+            List<GitRepositoryTable> tempList = new List<GitRepositoryTable>();
+
+            string query = "select * from Repository";
+            SQLiteCommand command = new SQLiteCommand(query, Connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int id = Convert.ToInt32(reader["ID"]);
+                string name = Convert.ToString(reader["Name"]);
+                string type = Convert.ToString(reader["Type"]);
+                GitRepositoryTable temp = new GitRepositoryTable(id, name, type);
+                tempList.Add(temp);
+            }
+            return tempList;
+        }
+
+        public int GetLastIndex(SQLiteConnection Connection)
+        {
+            int id = 0;
+            string query = "select ID from Repository";
+            SQLiteCommand command = new SQLiteCommand(query, Connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                id = Convert.ToInt32(reader["ID"]);
+            }
+            return id;
+        }
+
+        public static string FixName(string Repository)
+        {
+            string output = Repository;
+            string pattern = @"((.*)\\(.*)\\)";
+            Regex r = new Regex(pattern);
+            Match m = r.Match(Repository);
+            if (m.Success)
+            {
+                if (m.Groups.Count >= 3)
+                {
+                    output = m.Groups[3].Value;
+                }
+            }
+            return output;
+        }
+    }
+}
