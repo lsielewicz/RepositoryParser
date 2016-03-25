ac617c05f678a36896150732ba74c99c59df5b45
Lukasz Sielewicz <lsielewicz@wi.zut.edu.pl>
2016-03-25 13:45:18 +0100

    Added: SharpSvn library

---
+++ RepositoryParser/RepositoryParser.Core/Interfaces/ISvnService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryParser.Core.Interfaces
+{
+    public interface ISvnService
+    {
+        string Path { get; set; }
+
+    }
+}
