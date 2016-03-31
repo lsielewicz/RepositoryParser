5aed668ddb2e51ba277bc9dfd06579a4666ab733
Lukasz Sielewicz <lsielewicz@wi.zut.edu.pl>
2016-03-31 20:47:42 +0200

    Added GitCloneBranch model

---
+++ RepositoryParser/RepositoryParser.Core/Models/GitCloneBranch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryParser.Core.Models
+{
+    public class GitCloneBranch
+    {
+        public string BranchName { get; set; }
+        public string OriginName { get; set; }
+        public string RemoteName { get; set; }
+        public bool IsAlreadyCloned { get; set; }
+
+        GitCloneBranch(string branchName, string originName, string remoteName, bool isCloned)
+        {
+            this.BranchName = branchName;
+            this.OriginName = originName;
+            this.RemoteName = remoteName;
+            this.IsAlreadyCloned = isCloned;
+        }
+    }
+}
